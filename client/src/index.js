141d199751d15c16538a0698b3cfcee99de0c344
AM_Myrick <amyrick.820@gmail.com>
2019-01-08 16:32:02 -0500

    adds Router to app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
@@ -20,7 +21,9 @@ const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
