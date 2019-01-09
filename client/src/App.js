64cb2467c0f076f3ea89c69f2952ece0e546b518
AM_Myrick <amyrick.820@gmail.com>
2019-01-09 14:46:27 -0500

    adds routing to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <UserProfile />
+        <Route path="/" component={UserProfile} />
       </div>
     );
   }
