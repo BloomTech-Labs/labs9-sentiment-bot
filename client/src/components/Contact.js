d4cdf643a43e9c503ef7c8ae62f2b853eb016e99
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2019-01-11 09:52:46 -0700

    scaffolding for landing page

---
+++ client/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur maxime facilis eius dicta nam ratione, sed expedita suscipit alias vel ipsum, laudantium dolor cum facere non aperiam, et nihil cumque!</p>
+    </div>
+  )
+}
+
+export default Contact
