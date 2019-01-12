23e059e0f83d7547f3ce0945632109d4bcebe31f
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2019-01-11 23:47:14 -0700

    added NavLinks to Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Contact from './components/Contact';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Route path="/" component={UserProfile} />
+      <BrowserRouter>
+      <div className="App">
+       <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </div>
+      </BrowserRouter>
     );
   }
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-   <nav className="nav-wrapper">
+   <nav className="nav-wrapper blue darken-3">
      <div className="container">
        <a className="brand-logo">Feelzy</a>
        <ul className="right">
-         <li><a href="/home">Home</a></li>
-         <li><a href="/about">About</a></li>
-         <li><a href="/contact">Contact</a></li>
+         <li><Link to="/">Home</Link></li>
+         <li><NavLink to="/about">About</NavLink></li>
+         <li><NavLink to="/contact">Contact</NavLink></li>
        </ul>
      </div>
    </nav>
