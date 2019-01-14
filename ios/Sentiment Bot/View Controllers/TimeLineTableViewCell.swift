3eae58b43d8b465ff52fc0229f02f2d3eee19b72
Scott Bennett <stt.btt@gmail.com>
2019-01-14 00:02:08 -0500

    Add and hook up tableview cells

---
+++ ios/Sentiment Bot/View Controllers/TimeLineTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  TimeLineTableViewCell.swift
+//  Sentiment Bot
+//
+//  Created by Scott Bennett on 1/13/19.
+//  Copyright © 2019 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var feelzNameLabel: UILabel!
+    @IBOutlet weak var feelzImageView: UIImageView!
+    
+    func setResponse(response: Response) {
+        dateLabel.text = formatter.string(from: response.date)
+        emojiLabel.text = response.emoji
+        feelzNameLabel.text = response.mood
+        feelzImageView.image = #imageLiteral(resourceName: "stevejobs.jpeg")
+        locationLabel.text = "New York, NY"
+    }
+    
+    lazy var formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
