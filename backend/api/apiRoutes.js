a874e8a7d24431021a9c095678a8e1663a347edc
Hargobind Atwal <ethaipath@gmail.com>
2019-01-14 16:44:10 -0800

    Well idk if we need this be here it is.

--- backend/api/apiRoutes.js
+++ backend/api/apiRoutes.js
@@ -2,6 +2,29 @@ const express = require('express')
 
 const router = express()
 
+const protected = (req, res, next) => {
+  const token = req.headers.authentication;
+  console.log(token)
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: 'invalid token' });
+      } else {
+        req.decodedToken = decodedToken;
+        next()
+      }
+    })
+  } else {
+    res.status(401).json({message: 'No token provided.'})
+  }
+}
+
+
+// ======== JWT flow ==============
+
+
+
+// ================================
 // 'sanity' endpoint
 router.get('/', (req, res) => {
   res.status(200).json({
@@ -9,7 +32,9 @@ router.get('/', (req, res) => {
   })
 })
 
-router.use('/users', require('./dataRoutes/usersRoutes'))
+router.use('/accounts', require('./accountRoutes/jwt'))
+
+router.use('/users', protected, require('./dataRoutes/usersRoutes'))
 router.use('/slack', require('./slack-bot'))
 
 module.exports = router;
