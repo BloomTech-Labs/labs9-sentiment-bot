11025a1251caf5b57d1648894794896f53fb7bc7
Hargobind Atwal <ethaipath@gmail.com>
2019-01-17 14:38:16 -0800

    removed material-ui button

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import { Redirect, Route, Router } from 'react-router-dom';
 import UserProfile from "./components/UserProfile";
 import { oAuthFlow } from './actions'
@@ -16,7 +16,7 @@ class App extends Component {
     return (
       <div>
 
-        <Button onClick={this.loginHandler} >Login</Button>
+        <button onClick={this.loginHandler} >Login</button>
         <Route path="/profile" render={props => (
           !auth.isAuthenticated() ? (
             <Redirect to="/" />
