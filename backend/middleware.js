108e5317a007237f067ff96c303d57968ec2c439
Hargobind Atwal <ethaipath@gmail.com>
2019-01-17 10:29:21 -0800

    added comments

--- backend/middleware.js
+++ backend/middleware.js
@@ -4,8 +4,12 @@ const helmet = require('helmet')
 const  cors = require('cors')
 
 module.exports = server => {
-  server.use(morgan('dev')) // logs http requests to console
-  server.use(cors()) // cross origin resource shareing (needs configuring)
-  server.use(helmet()) // sets security headers
-  server.use(express.json()) // parses http body into json
+  // logs http requests to console
+  server.use(morgan('dev')) 
+  // enable requests from any domain
+  server.use(cors()) 
+  // sets security headers
+  server.use(helmet()) 
+  // parses http body into json
+  server.use(express.json()) 
 }
