c4faa45ca2e461fa73421f52061b7b40a78db0b9
Scott Bennett <stt.btt@gmail.com>
2019-01-24 10:32:02 -0500

    Add time picker

--- ios/Sentiment Bot/View Controllers/UserContainerViewController.swift
+++ ios/Sentiment Bot/View Controllers/UserContainerViewController.swift
@@ -21,28 +21,44 @@ class UserContainerViewController: UIViewController, UINavigationControllerDeleg
     var users: User?
     var team: Team?
     
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         userImage.layer.cornerRadius = userImage.frame.size.width / 2
         userImage.clipsToBounds = true
+        userImage.layer.borderWidth = 3.0
+        userImage.layer.borderColor = UIColor(displayP3Red: 132/256, green: 13/256, blue: 27/256, alpha: 0.5).cgColor
         
-        // TODO: - Should this be in awakeFromNib?
+        setupView()
+    }
+    
+    func setupView() {
         
         APIController.shared.getUser(userId: UserDefaults.standard.userId) { (users, error) in
             self.users = users
-            DispatchQueue.main.async {
-                guard let firstName = users?.firstName, let lastName = users?.lastName, let userID = users?.id else { return }
-                self.nameLabel.text = "\(firstName) \(lastName) (\(userID))"
-                
-                if let imageUrl = users?.imageUrl {
-                    APIController.shared.getImage(url: imageUrl) { (image, error) in
-                        if let error = error {
-                            NSLog("Error getting image \(error)")
-                        } else if let image = image {
-                            DispatchQueue.main.async {
-                                self.userImage.image = image
-                                
+            if let error = error {
+                NSLog("Error getting user: \(error)")
+            } else {
+                DispatchQueue.main.async {
+                    guard let firstName = users?.firstName, let lastName = users?.lastName, let userID = users?.id else { return }
+                    self.nameLabel.text = "\(firstName) \(lastName) (\(userID))"
+                    
+                    if let imageUrl = users?.imageUrl {
+                        APIController.shared.getImage(url: imageUrl) { (image, error) in
+                            if let error = error {
+                                NSLog("Error getting image \(error)")
+                            } else if let image = image {
+                                DispatchQueue.main.async {
+                                    self.userImage.image = image
+                                    
+                                }
                             }
                         }
                     }
@@ -52,35 +68,35 @@ class UserContainerViewController: UIViewController, UINavigationControllerDeleg
         
         APIController.shared.getUserResponses(userId: UserDefaults.standard.userId) { (responses, error) in
             self.responses = responses
-            DispatchQueue.main.async {
-                self.feelzNumberLabel.text = "Feelz: \(self.responses?.count ?? 0)"
-                self.lastInLabel.text = "Last In: \(responses?.last?.date ?? " ")"
+            if let error = error {
+                NSLog("Error getting user responses: \(error)")
+            } else {
+                DispatchQueue.main.async {
+                    self.feelzNumberLabel.text = "Feelz: \(self.responses?.count ?? 0)"
+                    self.lastInLabel.text = "Last In: \(responses?.last?.date ?? " ")"
+                }
             }
         }
         
         APIController.shared.getManagingTeam(userId: UserDefaults.standard.userId) { (team, error) in
             self.team = team
-            
-            DispatchQueue.main.async {
-                guard let admin = self.users?.isAdmin else { return }
-                if admin {
-                    self.teamIDLabel.text = "Team ID: \(team?.code ?? 0)"
-                } else {
-                    self.teamIDLabel.text = ""
+            if let error = error {
+                NSLog("Error getting managing team: \(error)")
+            } else {
+                DispatchQueue.main.async {
+                    guard let admin = self.users?.isAdmin else { return }
+                    if admin {
+                        self.teamIDLabel.text = "Team ID: \(team?.code ?? 0)"
+                    } else {
+                        self.teamIDLabel.text = ""
+                    }
                 }
             }
         }
     }
     
-    lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
+    // MARK: - Load Profile Picture
     @IBAction func selectImage(_ sender: UIButton) {
-        NSLog("Select Image")
         
             let vc = UIImagePickerController()
             if UIImagePickerController.isSourceTypeAvailable(.camera) {
@@ -105,7 +121,7 @@ class UserContainerViewController: UIViewController, UINavigationControllerDeleg
         
         APIController.shared.uploadProfilePicture(imageData: imageData!) { (error) in
             if let error = error {
-                NSLog("Error uploading: \(error)")
+                NSLog("Error uploading profile picture: \(error)")
                 return
             }
         }
