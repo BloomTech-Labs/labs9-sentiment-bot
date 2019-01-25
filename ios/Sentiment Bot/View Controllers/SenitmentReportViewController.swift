dc7bb5dc0f0bba3649804cd986d245eec3f27a9d
Moin Uddin <U_Moin75@yahoo.com>
2019-01-24 22:10:56 -0500

    Implement Pie Chart providing statistics of team sentiment

--- ios/Sentiment Bot/View Controllers/SenitmentReportViewController.swift
+++ ios/Sentiment Bot/View Controllers/SenitmentReportViewController.swift
@@ -7,16 +7,74 @@
 //
 
 import UIKit
+import Charts
 
-class SenitmentReportViewController: UIViewController {
-
+class SenitmentReportViewController: UIViewController, ManagerProtocol {
+    
+    var user: User?
+    
+    var teamResponses: [Response]?
+    
+    var team: Team?
+    
+    var survey: Survey?
+    
+    var teamMembers: [User]?
+    
+    @IBOutlet weak var pieChart: PieChartView!
+    
+        var emojiSelection: [String] = ["😄" ,"😃","😢","😊","😞", "😡"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let teamResponses = teamResponses else {
+            NSLog("teamResponses wasn't set on SentimentReportViewController")
+            return
+        }
+        
+        
+        for emoji in emojiSelection {
+            let filteredEmojis = teamResponses.filter({ $0.emoji == emoji})
+            
+            let chartDataEntry = PieChartDataEntry(value: Double((Double(filteredEmojis.count)/Double(teamResponses.count))*100.0))
+            chartDataEntry.label = emoji
+            
+            sentimentEntries.append(chartDataEntry)
+        }
+        
+        pieChart.chartDescription?.text = ""
+        
+        
+        updateChartData()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        pieChart.notifyDataSetChanged()
+    }
+    
+    
+    
+    var sentimentEntries: [PieChartDataEntry] = []
 
+    
+    func updateChartData() {
+        
+        let chartDataSet = PieChartDataSet(values: sentimentEntries, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        formatter.maximumFractionDigits = 1
+        formatter.multiplier = 1.0
+        chartData.setValueFormatter(DefaultValueFormatter(formatter:formatter))
+        
+        let colors = [UIColor.blue, UIColor.orange, UIColor.red, UIColor.lightGray, UIColor.green, UIColor.magenta]
+        
+        chartDataSet.colors = colors as! [NSUIColor]
+        
+        pieChart.data = chartData
+    }
     /*
     // MARK: - Navigation
 
