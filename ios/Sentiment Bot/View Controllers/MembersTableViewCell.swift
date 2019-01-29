cbcd5729f4b8168d85947b90ea77cfefde30f3b3
Scott Bennett <stt.btt@gmail.com>
2019-01-29 15:23:49 -0500

    Update tableviewcells

---
+++ ios/Sentiment Bot/View Controllers/MembersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MembersTableViewCell.swift
+//  Sentiment Bot
+//
+//  Created by Scott Bennett on 1/29/19.
+//  Copyright © 2019 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class MembersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
